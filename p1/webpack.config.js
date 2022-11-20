590c9690c64e53d8065452769f91e0792a33c529
Alex Townson <59401739+alextownson@users.noreply.github.com>
2022-11-20 08:37:56 -0500

    setup webpack for p1

---
+++ p1/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development', 
+    entry: './src/index.js', 
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg|)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+        open: true
+    }
+}
