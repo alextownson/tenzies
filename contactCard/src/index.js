8565e44fdd299b07dabbb2a92140d333bf2d4392
Alex Townson <59401739+alextownson@users.noreply.github.com>
2022-12-21 18:37:27 -0500

    separate star component

--- contactCard/src/index.js
+++ contactCard/src/index.js
@@ -1,8 +1,7 @@
 import './main.css'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import starNoFill from './assets/star-01.png'
-import starFill from './assets/star-02.png'
+import Star from './components/Star'
 
 function App() {
 
@@ -13,8 +12,6 @@ function App() {
         email: "alexanderlevi@gmail.com",
         isFavourite: false
     })
-
-    const starIcon = contact.isFavourite ? starFill : starNoFill
 
     function toggleFavourite() {
         setContact(prevContact => {
@@ -29,7 +26,7 @@ function App() {
         <main>
             <article>
                 <div className='card--image'></div>
-                <img onClick={toggleFavourite} className='card--star' src={starIcon} />
+                <Star isFilled={contact.isFavourite} handleClick={toggleFavourite} />
                 <h2 className='card--name'>{contact.firstName} {contact.lastName}</h2>
                 <p className='card--contact'>{contact.phone}</p>
                 <p className='card--contact'>{contact.email}</p>
