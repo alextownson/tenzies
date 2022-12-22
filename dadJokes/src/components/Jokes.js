4d9e9f593ad47d4a9ac122663dd020bed3f9241d
Alex Townson <59401739+alextownson@users.noreply.github.com>
2022-12-22 17:02:31 -0500

    conditional practice

--- dadJokes/src/components/Jokes.js
+++ dadJokes/src/components/Jokes.js
@@ -1,8 +1,19 @@
+import React from 'react'
+
 export default function Jokes(props) {
+    
+    const [isShown, setIsShown] = React.useState(false)
+
+    function toggle() {
+        setIsShown(prevState => !prevState)
+    } 
+
     return (
         <div className="joke">
             <p><strong>{props.setup}</strong></p>
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggle}>{isShown ? 'Hide Punchline' : 'Show Punchline'}</button>
+            <hr />
         </div>
     )
 }
