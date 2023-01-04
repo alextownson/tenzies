a1fd9b3e7b9799e15a00134f43b08b0fca76e108
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-01-04 13:50:18 -0500

    dark mode

---
+++ p4-reactFacts-darkMode/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import React from 'react'
+
+export default function App() {
+
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleDarkMode() {
+        setDarkMode(prevDarkMode => !prevDarkMode)
+    }
+
+    return(
+        <div className='container'>
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Main darkMode={darkMode}/>
+        </div>
+    )
+}
